b310b529ffe9e0f63e08d0691999d73c097e34e2
EmperorXSama <5alidghoddane@protonmail.com>
2026-06-09 13:37:50 +0100

    add /api

--- src/Filo.Ui/src/services/index.ts
+++ src/Filo.Ui/src/services/index.ts
@@ -2,6 +2,6 @@ import { apiClient } from '@/lib/axios'
 import type { SystemInfo } from '@/types'
 
 export async function getSystemInfo(): Promise<SystemInfo> {
-  const { data } = await apiClient.get<SystemInfo>('system-info')
+  const { data } = await apiClient.get<SystemInfo>('/api/system-info')
   return data
 }
